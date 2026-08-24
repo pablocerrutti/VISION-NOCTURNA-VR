c9128ac1cca3bacc5ac71bc2a6888017bd1c649f
pablocerrutti <pablocerrutti@gmail.com>
2026-08-24 11:10:36 -0300

    Delete js directory

--- js/vr.js
+++
@@ -1,5 +0,0 @@
-window.LW=window.LW||{};
-LW.vr={draw(ctx,work,w,h,s){
-  const gap=w*s.vrSeparation,ew=(w-gap)/2,sc=s.vrScale,dw=ew*sc,dh=h*sc,y=(h-dh)/2;
-  ctx.drawImage(work,(ew-dw)/2,y,dw,dh);ctx.drawImage(work,ew+gap+(ew-dw)/2,y,dw,dh);
-}};
