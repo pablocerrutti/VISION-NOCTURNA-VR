05eefb1ee9e019dfe4e0749250a498c64adf18c1
pablocerrutti <pablocerrutti@gmail.com>
2026-08-24 11:16:45 -0300

    Add files via upload

---
+++ js/vr.js
@@ -0,0 +1,14 @@
+window.LW=window.LW||{};
+LW.vr={
+ setHUD(on){document.querySelectorAll('.vr-hud').forEach(el=>el.classList.toggle('active',!!on))},
+ draw(ctx,work,w,h,s){
+   const gap=w*s.vrSeparation,ew=(w-gap)/2,sc=s.vrScale,dw=ew*sc,dh=h*sc,y=(h-dh)/2;
+   ctx.drawImage(work,(ew-dw)/2,y,dw,dh);ctx.drawImage(work,ew+gap+(ew-dw)/2,y,dw,dh);
+ },
+ drawOverlay(ctx,w,h,s,p){
+   const gap=w*s.vrSeparation,ew=(w-gap)/2;
+   const lp={...s,point:p?{x:p.x/2,y:p.y}:null},rp={...s,point:p?{x:ew+gap+p.x/2,y:p.y}:null};
+   ctx.save();ctx.beginPath();ctx.rect(0,0,ew,h);ctx.clip();LW.reticle.draw(ctx,ew,h,lp);ctx.restore();
+   ctx.save();ctx.beginPath();ctx.rect(ew+gap,0,ew,h);ctx.clip();LW.reticle.draw(ctx,ew+gap,h,rp);ctx.restore();
+ }
+};
