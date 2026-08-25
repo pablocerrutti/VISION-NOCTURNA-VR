4da0f10b89528ab1428cadfadf38b5aa3d590130
pablocerrutti <pablocerrutti@gmail.com>
2026-08-25 20:20:21 -0300

    Invalidate offline cache for VR distance HUD fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='lonewolf-nightvision-v19-distance-ruler';
+const CACHE_NAME='lonewolf-nightvision-v20-vr-distance-hud';
 const SHELL=['./','./index.html','./css/style.css','./js/camera.js','./js/image-processing.js','./js/measurement-v2.js','./js/measurement-gesture.js','./js/reticle.js','./js/reticle-mildot.js','./js/measurement-frame.js','./js/vr.js','./js/compass-v2.js','./js/vr-control.js','./js/app-v2.js','./manifest.json','./assets/icon-192.jpg','./assets/icon-512.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
