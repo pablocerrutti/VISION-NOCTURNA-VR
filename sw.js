25381009835e4d55547943a2376e5e6f75405cb8
pablocerrutti <pablocerrutti@gmail.com>
2026-08-25 18:04:23 -0300

    Update offline cache for camera permission fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='lonewolf-nightvision-v10.0-mildot-76x45';
+const CACHE_NAME='lonewolf-nightvision-v11-camera-permissions';
 const SHELL=['./','./index.html','./css/style.css','./js/camera.js','./js/image-processing.js','./js/measurement-v2.js','./js/measurement-gesture.js','./js/reticle.js','./js/reticle-mildot.js','./js/measurement-frame.js','./js/vr.js','./js/compass-v2.js','./js/vr-control.js','./js/app-v2.js','./manifest.json','./assets/icon-192.jpg','./assets/icon-512.jpg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
