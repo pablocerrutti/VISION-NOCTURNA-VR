458e9e297d76024f1da4702e161dc24674b5dae5
pablocerrutti <pablocerrutti@gmail.com>
2026-08-25 15:57:22 -0300

    Actualizar caché offline para versión Mil-Dot 76x45

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='lonewolf-nightvision-v9.0.0-landscape-150m-autoheight';
-const SHELL=['./','./index.html','./css/style.css','./js/camera.js','./js/image-processing.js','./js/measurement-v2.js','./js/measurement-gesture.js','./js/reticle.js','./js/vr.js','./js/compass-v2.js','./js/vr-control.js','./js/app-v2.js','./manifest.json','./assets/icon-192.jpg','./assets/icon-512.jpg'];
+const CACHE_NAME='lonewolf-nightvision-v10.0-mildot-76x45';
+const SHELL=['./','./index.html','./css/style.css','./js/camera.js','./js/image-processing.js','./js/measurement-v2.js','./js/measurement-gesture.js','./js/reticle.js','./js/reticle-mildot.js','./js/measurement-frame.js','./js/vr.js','./js/compass-v2.js','./js/vr-control.js','./js/app-v2.js','./manifest.json','./assets/icon-192.jpg','./assets/icon-512.jpg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting()});
