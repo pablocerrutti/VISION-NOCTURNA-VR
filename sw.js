84c905f98faa700f696a0d32b88afde4dc618566
pablocerrutti <pablocerrutti@gmail.com>
2026-08-26 14:29:17 -0300

    Update offline cache for 10-100m range states

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='lonewolf-nightvision-v26-range-10-100';
+const CACHE_NAME='lonewolf-nightvision-v27-range-10-100';
 const SHELL=['./','./index.html','./css/style.css','./js/camera.js','./js/image-processing.js','./js/measurement-v2.js','./js/measurement-gesture.js','./js/reticle.js','./js/measurement-frame.js','./js/vr.js','./js/compass-v2.js','./js/vr-control.js','./js/app-v2.js','./manifest.json','./assets/icon-192.jpg','./assets/icon-512.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
